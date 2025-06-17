bc689155ed8b4994f871e622d5df329f2b7a1372
juwitardiyanti <123800463+juwitardiyanti@users.noreply.github.com>
2025-06-17 19:07:36 +0700

    Create quiz1-basic-variables.js

---
+++ quiz1-basic-variables.js
@@ -0,0 +1,16 @@
+// Membuat variabel (konstan) bernama `currency` dan isi dengan nilai "IDR".
+const currency = "IDR";
+
+// Membuat variabel bernama `value` dan isi dengan nilai 10000.
+let value = 10000;
+
+// Menambahkan nilai di dalam variabel `value` sebesar 5000.
+value += 5000;
+
+/**
+ * Membuat variabel (konstan) bernama `money`,
+ * Mengisi dengan penambahan string dari nilai `currency` + " " + `value`.
+ */
+const money = currency + " " + value;
+
+console.log(money); 
